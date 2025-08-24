ab1aae386f59d5e572d218204458df78753ae63f
chitrakshi-222 <chitrakshigulia@gmail.com>
2025-08-24 06:02:58 +0530

    solved some bugs

--- blooms/src/conf/conf.js
+++ blooms/src/conf/conf.js
@@ -3,7 +3,7 @@ const conf = {
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    token : import.meta.env.VITE_HUGGINGFACE_TOKEN,
+    
     appwriteReviewSessionsCollectionId: String(import.meta.env.VITE_APPWRITE_REVIEW_SESSIONS_COLLECTION_ID),
     appwriteApiKey:import.meta.env.VITE_API_KEY,
 
